196370116b29800338fb79ef6ab1840b3a741bae
mrheld <markus.held@chalmers.se>
2023-04-05 11:13:32 +0200

    added matrixexp_b and first small gauss-laguerre quadrature table

--- inc/dg/helmholtz_b.cu
+++ inc/dg/helmholtz_b.cu
@@ -14,7 +14,7 @@
 
 
 
-const double eps = 1e-2;
+const double eps = 1e-12;
 const double alpha = -0.5;
 double lhs( double x, double y){ return sin(x)*sin(y);}
 double rhs( double x, double y){ return (1.-2.*alpha)*sin(x)*sin(y);}
