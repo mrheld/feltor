4dcb4380e0516845afa60768a9dfd779ffc738af
mrheld <markus.held@chalmers.se>
2023-04-18 09:05:11 +0200

    added adaptive gauss-legendre quadrature that adapts to bessel zeros

--- inc/matrix/matrixexp_b.cu
+++ inc/matrix/matrixexp_b.cu
@@ -20,19 +20,8 @@ const double alpha = 0.5;
 const double ell_fac = (m*m+n*n);
 
 std::vector<double> BesselJ0Zeros = 
-{
-	0.0, //not a zero but for grid
-	2.404825557695772768621631879326454643124,
-	5.520078110286310649596604112813027425222,
-	8.653727912911012216954198712660946685566,
-	11.79153443901428161374304491192545892202,
-	14.93091770848778594776259399738868220792,
-	18.07106396791092254314788297561817656025,
-	21.21163662987925895907839335052630683618,
-	24.35247153074930273705794476317890718457,
-	27.49347913204025479587728823460741454653,
-	30.63460646843197511754957892685423273727
-};
+{0.0,2.404825557695772768621631879326454643124,5.520078110286310649596604112813027425222,8.653727912911012216954198712660946685566,11.79153443901428161374304491192545892202,14.93091770848778594776259399738868220792,18.07106396791092254314788297561817656025,21.21163662987925895907839335052630683618,24.35247153074930273705794476317890718457,27.49347913204025479587728823460741454653,30.63460646843197511754957892685423273727,33.77582021357356868423854634671472802393,36.91709835366404397976949306327295263649,40.05842576462823929479930737399447291035,43.19979171317673035752407272874342217088,46.34118837166181401868578887911284917465,49.48260989739781717360276153317827226970,52.62405184111499602925128538039157330012,55.76551075501997931168349277346183063138,58.90698392608094213283440663461568558566,62.04846919022716988285250026465095232382,65.18996480020686044063603374251231622566,68.33146932985679827099230383998437803685,71.47298160359373282506307385612970055337,74.61450064370183788382054046933555420302,77.75602563038805503773937189123369326508,80.89755587113762786377214349087305285687,84.03909077693819015787963834799795512771,87.18062984364115365126180506904860884867,90.32217263721048005571776677762790891449,93.46371878194477417119059154398091075732,96.60526795099626877812161732392789687943,99.74681985868059647027997900013457780104,102.8883742541947945964200342725645762893,106.0299309164516155101769171918824475816,109.1714896498053835520659770127496566183,112.3130502804949096274945061221783313859,115.4546126536669396281177566940295965973,118.5961766308725317156293844752439096603,121.7377420879509629652343634833408945103,124.8793089132329460452591283669041436285};
+
 
 double lhs( double x, double y){ return sin(x*m)*sin(y*n);}
 
@@ -79,7 +68,7 @@ struct POW
         double m_power;
 };
 
-///@brief \f$ f(x) = exp(-x)*LaguerreL(n,x)*BesselJ(0,2*Sqrt(x*t))\f$
+///@brief \f$ f(x) = exp(-x)*LaguerreL(n,x)*BesselJ(0,Sqrt(4.0*x*t))\f$
 template<class T = double>
 struct Gyrointegrant
 {
@@ -87,7 +76,7 @@ struct Gyrointegrant
 
     DG_DEVICE T  operator()(double x) const
     {
-        return exp(-x)*boost::math::laguerre(m_n, x)*boost::math::cyl_bessel_j(0, 2.0*sqrt(x*m_t));
+        return exp(-x)*boost::math::laguerre(m_n, x)*boost::math::cyl_bessel_j(0, sqrt(4.0*x*m_t));
     }
   private:
     double  m_t;
@@ -171,12 +160,12 @@ int main(int argc, char * argv[])
 	},
        //exp with elliptic via GLeg quadrature
         [](double x) {            
-            const unsigned GLeg_n = 3;
-            const unsigned GLeg_Nx = 3;
+            const unsigned GLeg_n = 19;
+            const unsigned GLeg_Nx = 30;
 	    double integral=0.0;
-            for (unsigned i=0; i<5; i++)
+            for (unsigned i=0; i<20; i++)
 	    {
-          	    dg::Grid1d g1d( BesselJ0Zeros[i]*sqrt(x*alpha), BesselJ0Zeros[i+1]*sqrt(x*alpha), GLeg_n, GLeg_Nx);
+          	    dg::Grid1d g1d( BesselJ0Zeros[i]*sqrt(x*alpha), BesselJ0Zeros[i+1]*sqrt(x*alpha), GLeg_n, GLeg_Nx); //Rescaling of zeros might be wrong!!!
                     Gyrointegrant<double> gyroint(x*alpha,0);
                     const dg::HVec f = dg::evaluate( gyroint, g1d); //f=exp(-t) BesselJ0(2*sqrt(t*alpha*x))
                     const dg::HVec w1d = dg::create::weights( g1d);
@@ -215,14 +204,14 @@ int main(int argc, char * argv[])
             "Inv(1+alpha A)",
             "K_0_GLag(-alpha A)",
             "K_0_GLagsep(-alpha A)",
-            "K_0_GLeg(-alpha A)"  
+            "K_0_GLegZ(-alpha A)"  
     };
     for( unsigned u=0; u<funcs.size(); u++)
     {
         std::cout << "\n#Compute x = "<<outs[u]<<" b " << std::endl;
 
         Container x = dg::evaluate(lhs, g), x_exac(x), b(x), error(x);
-        if (u==5 || u==6 ) dg::blas1::scal(x_exac, funcs[0](ell_fac)); //use exact exp(-alpha A) for GLag and GLeg
+        if (u==5 || u==6 || u==7) dg::blas1::scal(x_exac, funcs[0](ell_fac)); //use exact exp(-alpha A) for GLag and GLeg
         else dg::blas1::scal(x_exac, funcs[u](ell_fac));
 
         double res_fac = kappa*funcs[u](EVmin);
