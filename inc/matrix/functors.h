ae473c0ddc42e83761c785c40a3a634c8015e8c8
mrheld <markus.held@chalmers.se>
2023-05-10 13:29:04 +0200

    worked on matrix prod implementation, added exact matrix prod solution

--- inc/matrix/functors.h
+++ inc/matrix/functors.h
@@ -23,6 +23,61 @@ struct BESSELI0
         return boost::math::cyl_bessel_i(0, x);
     }
 };
+
+/**
+ * @brief \f$ f(x) = J_n (x)\f$ with \f$J_n\f$ the n-th order modified Bessel function
+ *
+ * @tparam T value type
+ */
+template< class T = double >
+struct BesselJ
+{
+    BesselJ(unsigned n): m_n(n) {}
+
+    DG_DEVICE T operator() ( T x) const
+    {
+        return boost::math::cyl_bessel_j(m_n, x);
+    }
+    private:
+    	unsigned m_n;
+};
+
+/**
+ * @brief \f$ f(x) = L_n (x)\f$ with \f$L_n\f$ the n-th order Laguerre polynomial
+ *
+ * @tparam T value type
+ */
+template< class T = double >
+struct LaguerreL
+{
+    LaguerreL(unsigned n): m_n(n) {}
+
+    DG_DEVICE T operator() ( T x) const
+    {
+        return boost::math::laguerre(m_n, x);
+    }
+    private:
+        unsigned m_n;
+};
+
+
+/**
+ * @brief \f$ f(x) = x^p\f$
+ *
+ * @tparam T value type
+ */
+template< class T = double >
+struct POW
+{
+    POW(double p): m_p(p) {}
+    DG_DEVICE T operator() (T x) const
+    {
+	    return pow(x, m_p);
+    }
+	private:
+        double m_p;
+};
+
 /**
  * @brief \f$ f(x) = \Gamma_0 (x) := I_0 (x) exp(x) \f$ with \f$I_0\f$ the zeroth order modified Bessel function
  *
@@ -46,6 +101,78 @@ struct GAMMA0
 };
 
 
+/**
+ * @brief \f$ f(x) = (-a*x)^n/n! exp(a*x) \f$ 
+ *
+ * @tparam T value type
+ */
+template<class T = double>
+struct GyrolagK
+{
+    GyrolagK(double n, double a): m_n(n), m_a(a) {}
+
+    DG_DEVICE T  operator()(double x) const { return pow(-x*m_a,m_n)/tgamma(m_n+1)*exp(x*m_a); }
+
+    private:
+        double  m_n, m_a;
+};
+
+/**
+ * @brief \f$ f(x) =exp(-x)*L_n(x)*J_0(Sqrt(4.0*x*t))\f$ 
+ *
+ * @tparam T value type
+ */
+template<class T = double>
+struct Gyrointegrant
+{
+    Gyrointegrant(double t, unsigned n):m_t(t), m_n(n) {}
+
+    DG_DEVICE T  operator()(double x) const
+    {
+        return exp(-x)*boost::math::laguerre(m_n, x)*boost::math::cyl_bessel_j(0, sqrt(4.0*x*m_t));
+    }
+  private:
+    double  m_t;
+    unsigned m_n;
+};
+
+/**
+ * @brief \f$ f(x) =x/2* exp(-x^2/4)*L_n(x^2/4)*J_0(x*Sqrt(t))\f$ 
+ *
+ * @tparam T value type
+ */
+template<class T = double>
+struct Gyrointegranttrans
+{
+    Gyrointegranttrans(double t, unsigned n):m_t(t), m_n(n) {}
+
+    DG_DEVICE T  operator()(double x) const
+    {
+        return x/2.0*exp(-x*x/4.)*boost::math::laguerre(m_n, x*x/4.)*boost::math::cyl_bessel_j(0, x*sqrt(m_t));
+    }
+  private:
+    double m_t;
+    unsigned m_n;
+};
+
+/**
+ * @brief \f$ f(x) =x/(2*t) exp(-x^2/(4*t))*L_n((x^2/(4*t))*J_0(x)\f$ 
+ *
+ * @tparam T value type
+ */
+template<class T = double>
+struct Gyrointegranttrans2
+{
+    Gyrointegranttrans2(double t, unsigned n):m_t(t), m_n(n) {}
+
+    DG_DEVICE T  operator()(double x) const
+    {
+        return x/(2.0*m_t)*exp(-x*x/(4.*m_t))*boost::math::laguerre(m_n, x*x/(4.*m_t))*boost::math::cyl_bessel_j(0, x);
+    }
+  private:
+    double m_t;
+    unsigned m_n;
+};
 
 }//namespace mat
 }//namespace dg
--- inc/matrix/matrixprodexp_b.cu
+++ inc/matrix/matrixprodexp_b.cu
@@ -17,7 +17,6 @@
 
 #include <cusp/lapack/lapack.h>
 
-
 const double lx = 2.*M_PI;
 const double ly = 2.*M_PI;
 dg::bc bcx = dg::DIR;
@@ -29,63 +28,6 @@ const double ell_fac = (m*m+n*n);
 
 double lhs( double x, double y){ return sin(x*m)*sin(y*n);}
 
-///@brief \f$ f(x) = BesselJ(n,x)\f$
-template< class T = double >
-struct BesselJ
-{
-    BesselJ(unsigned n): m_n(n) {}
-
-    DG_DEVICE T operator() ( T x) const
-    {
-        return boost::math::cyl_bessel_j(m_n, x);
-    }
-    private:
-    	unsigned m_n;
-};
-
-///@brief \f$ f(x) = LaguerreL(n,x)\f$
-template< class T = double >
-struct LaguerreL
-{
-    LaguerreL(unsigned n): m_n(n) {}
-
-    DG_DEVICE T operator() ( T x) const
-    {
-        return boost::math::laguerre(m_n, x);
-    }
-    private:
-        unsigned m_n;
-};
-
-///@brief \f$ f(x) = (-x)^n/n! exp(x)\f$
-double GyroLagK(int n, double x) { return pow(-x,n)/tgamma(n+1)*exp(x);}
-
-///@brief \f$ f(x) = (-a*x)^n/n! exp(a*x)\f$
-template<class T = double>
-struct GyrolagKop
-{
-    GyrolagKop(int n, double a): m_n(n), m_a(a) {}
-
-    DG_DEVICE T  operator()(double x) const { return pow(-x*m_a,m_n)/tgamma(m_n+1)*exp(x*m_a); }
-  private:
-    double  m_a;
-    unsigned m_n;
-};
-
-///@brief \f$ f(x) = exp(-x)*LaguerreL(n,x)*BesselJ(0,2*Sqrt(x*t))\f$
-template<class T = double>
-struct Gyrointegrant
-{
-    Gyrointegrant(double t, unsigned n):m_t(t), m_n(n) {}
-
-    DG_DEVICE T  operator()(double x) const
-    {
-        return exp(-x)*boost::math::laguerre(m_n, x)*boost::math::cyl_bessel_j(0, 2.0*sqrt(x*m_t));
-    }
-  private:
-    double  m_t;
-    unsigned m_n;
-};
 using Matrix = dg::DMatrix;
 using Container = dg::DVec;
 
@@ -122,21 +64,17 @@ int main(int argc, char * argv[])
     std::cout << "#   min(W)  = "<<min_weights <<"  max(W) = "<<max_weights << "\n";
     const double kappa = sqrt(max_weights/min_weights); //condition number
     dg::Elliptic<dg::CartesianGrid2d, Matrix, Container> A( {g, dg::centered, 1.0});
-    dg::mat::UniversalLanczos<Container> lanczos( A.weights(), 20);
-    auto T = lanczos.tridiag( A, A.weights(), A.weights());
-    auto extremeEVs = dg::mat::compute_extreme_EV( T);
-    double EVmin = extremeEVs[0];
-    double EVmax = extremeEVs[1];
-
 
     std::vector< std::function<double (double)>> funcs{
-	//Elliptic
-        [](double x) { return GyroLagK(0,-alpha*x);},
-        [](double x) { return GyroLagK(0,-alpha*x);}
+        [](double x) { return dg::mat::GyrolagK<double>(0.,-alpha)(x);},
+        [](double x) { return dg::mat::GyrolagK<double>(0.,-alpha)(x);},
+        [](double x) { return dg::mat::GyrolagK<double>(0.,-alpha)(x);}        
     };
     std::vector<std::string> outs = {
-            "K_0_naive(-alpha A)",
-            "K_0_fast(-alpha A)"
+            "K_0(-alpha A)",
+            "K_0_prod(-alpha A)",
+            "K_0_prodadj(-alpha A)"
+//             "K_0_naive(-alpha A)"
     };
     for( unsigned u=0; u<funcs.size(); u++)
     {
@@ -144,32 +82,31 @@ int main(int argc, char * argv[])
 
         Container x = dg::evaluate(lhs, g), x_exac(x), x_h(x), b(x), error(x);
         Container d = dg::evaluate(dg::ONE(), g);
-        dg::blas1::scal(x_exac, funcs[u](ell_fac));
+//         Container d = dg::evaluate(dg::SinXSinY(0.99, 1.0, 1, 1), g);
+        
         
 
         std::cout << outs[u] << ":\n";
 
         dg::mat::UniversalLanczos<Container> krylovfunceigen( x, max_iter);
+        dg::mat::UniversalLanczos<Container> krylovfunceigend( x, max_iter);
 
         auto func = dg::mat::make_FuncEigen_Te1( funcs[u]);
         double time = t.diff();
         unsigned iter_sum=0;
 	//MLanczos-universal
         
+
         if (u==0)
         {
             t.tic();
-            for( unsigned k=0; k<x.size(); k++)
-            {
-//                 A.set_chi(lambda_d[k]);
-                iter= krylovfunceigen.solve(x_h, func, A, b, w2d, eps, 1., "universal");
-//                 std::cout << "    universal-iter[k]: "<<std::setw(3)<<iter <<"[" << k <<"]"<< "\n";
-                iter_sum+=iter;
-                x[k] = x_h[k];
-            }
+            iter= krylovfunceigen.solve(x, func, A, b, w2d, eps, 1., "universal");
             t.toc();
             time = t.diff();
-            std::cout << "    universal-iter_sum: "<<std::setw(3)<<iter_sum << "\n";
+            std::cout << "    universal-iter: "<<std::setw(3)<<iter << "\n";
+            
+            dg::blas1::scal(x_exac, funcs[u](ell_fac));
+
         }
         if (u==1)
         {
@@ -194,40 +131,98 @@ int main(int argc, char * argv[])
 
             for( unsigned l=0; l<iter; l++)
             {
-//              std::cout << "l " <<l << std::endl;
                 dg::blas1::copy( 0, c[l]); // init sum
                 dg::blas1::copy( 0, v[l]); // init sum
                 //e_l
                 e_l[l] = 1.;
                 if (l>0) e_l[l-1]=0.;
                 //Compute c[l]
-                std::cout << "cl \n";
                 for( unsigned j=0; j<iter; j++)
                 {
-                    std::cout << "j " << j <<  "   " << evals[j] << std::endl;
-                    dg::blas1::axpby( evals[j], d, 0., fd);                        
-                    dg::blas1::transform(fd, fd, GyrolagKop<double>(0,-1.*alpha));
+                    dg::blas1::axpby( evals[j], d, 0., fd);
+                    dg::blas1::transform(fd, fd, dg::mat::GyrolagK<double>(0.,-alpha));
                     dg::blas1::axpby( evecs(0,j)*evecs(l,j), fd, 1., c[l]);
                 }
-                std::cout << "vl \n";
                 //compute v[l]
                 krylovfunceigen.normMbVy(A, Tf, e_l, v[l], b, krylovfunceigen.get_bnorm()); //v[l]=  ||b|| V e_l
-                std::cout << "x \n";
 
                 //compute x+=v[l] p. c[l]
                 dg::blas1::pointwiseDot(1.0, v[l], c[l], 1.0, x);
-                std::cout << " after x \n";
-
             }
-            std::cout << "x2 \n";
-
             t.toc();
             time = t.diff();
-                        std::cout << "x3 \n";
+            
+            //Compute exact error for product exponential
+            dg::blas1::axpby(ell_fac, d, 0.0, fd);
+            dg::blas1::transform(fd, fd, dg::mat::GyrolagK<double>(0.,-alpha));
+            dg::blas1::pointwiseDot(fd, x_exac, x_exac); //f(-alpha*(m^2+n^2) d) sin(m x) cos(n y)
+        }
+        if (u==2)
+        {
+            t.tic();
+            //Tridiagonalize diagonal matrix D
+            auto Rf = krylovfunceigend.tridiag(func, d,  b, w2d, eps, 1.,  "universal");
+            unsigned iter_Rf = krylovfunceigend.get_iter();
+            std::cout << "    universal-iter-Rf: "<<std::setw(3)<< iter_Rf << "\n";
+//             cusp::print(Rf);
+            
+            //make eigendecomposition of Rf = E_Rf  eval_Rf E_Rf^T 
+            cusp::array2d< double, cusp::host_memory> evecs_Rf(iter_Rf,iter_Rf);
+            cusp::array1d< double, cusp::host_memory> evals_Rf(iter_Rf);
+            cusp::lapack::stev(Rf.values.column(1), Rf.values.column(2), evals_Rf, evecs_Rf, 'V');
+            cusp::coo_matrix<int, double, cusp::host_memory> E_Rf, E_Rf_t;
+            cusp::convert(evecs_Rf, E_Rf);
+            cusp::transpose(E_Rf, E_Rf_t);
+            
+            
+            //Compute h_k
+            dg::HVec e_1(iter_Rf,0.), e_k(e_1), y(e_1); //unit vector e_1
+            std::vector<dg::HVec> h{iter_Rf, e_1};
+            std::vector<Container> v{iter_Rf, d}; 
+            e_1[0] = 1.;
+            
+            dg::blas2::symv(E_Rf_t, e_1, y); //y = E_Rf^T e_1
+            dg::blas1::scal(x,0.0);
+            for( unsigned k=0; k<iter_Rf; k++)
+            {
+                e_k[k] = 1.;
+                if (k>0) e_k[k-1]=0.;
+                
+                dg::blas1::pointwiseDot(e_k, y, y); //y = e_k * (E_Rf^T e_1)
+                dg::blas2::symv(E_Rf, y, h[k]);
+                
+                krylovfunceigend.normMbVy(d, Rf, h[k], v[k], b, krylovfunceigend.get_bnorm()); //v[k]=  ||b|| V_Rf h[k]
+                
+                //Solve 
+                iter= krylovfunceigen.solve(x_h, func, A, v[k], w2d, eps, 1., "universal"); // x_h = ||v_k|| V_Tf f(Tf lambda_Rf,k) v[k]
+                dg::blas1::axpby(1.0, x_h, 1.0, x);
+                std::cout << "    universal-iter-Tf: "<<std::setw(3)<< krylovfunceigen.get_iter() << "\n";
+
+            }
+            
+            //Tridiagonalize A first to T with the stopping condition for the function exp(-max(d)*alpha A)
+            t.toc();
+            time = t.diff();
+            
+            x_exac = dg::evaluate(lhs, g);
+            dg::blas1::scal(x_exac, funcs[u](ell_fac));
 
         }
-                        std::cout << "er \n";
-
+//         if (u==3)
+//         {
+//             t.tic();
+//             for( unsigned k=0; k<x.size(); k++)
+//             {
+// //                 A.set_chi(lambda_d[k]);
+//                 iter= krylovfunceigen.solve(x_h, func, A, b, w2d, eps, 1., "universal");
+// //                 std::cout << "    universal-iter[k]: "<<std::setw(3)<<iter <<"[" << k <<"]"<< "\n";
+//                 iter_sum+=iter;
+//                 x[k] = x_h[k];
+//             }
+//             t.toc();
+//             time = t.diff();
+//             std::cout << "    universal-iter_sum: "<<std::setw(3)<<iter_sum << "\n";
+//         }
         dg::blas1::axpby(1.0, x, -1.0, x_exac, error);
         erel = sqrt(dg::blas2::dot( w2d, error) / dg::blas2::dot( w2d, x_exac));
 
