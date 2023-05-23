398dc61433e3511d7a3a433905ad9950a4ee0d7e
mrheld <markus.held@chalmers.se>
2023-05-23 13:58:12 +0200

    implemented fast adjint flr op, but does not approximate exact solution

--- inc/matrix/matrixprodexp_b.cu
+++ inc/matrix/matrixprodexp_b.cu
@@ -81,10 +81,9 @@ int main(int argc, char * argv[])
         std::cout << "\n#Compute x = "<<outs[u]<<" b " << std::endl;
 
         Container x = dg::evaluate(lhs, g), x_exac(x), x_h(x), b(x), error(x);
+        Container one = dg::evaluate(dg::ONE(), g);
         Container d = dg::evaluate(dg::ONE(), g);
 //         Container d = dg::evaluate(dg::SinXSinY(0.99, 1.0, 1, 1), g);
-        
-        
 
         std::cout << outs[u] << ":\n";
 
@@ -95,8 +94,6 @@ int main(int argc, char * argv[])
         double time = t.diff();
         unsigned iter_sum=0;
 	//MLanczos-universal
-        
-
         if (u==0)
         {
             t.tic();
@@ -160,59 +157,145 @@ int main(int argc, char * argv[])
         if (u==2)
         {
             t.tic();
-            //Tridiagonalize diagonal matrix D
-            auto Rf = krylovfunceigend.tridiag(func, d,  b, w2d, eps, 1.,  "universal");
-            unsigned iter_Rf = krylovfunceigend.get_iter();
-            std::cout << "    universal-iter-Rf: "<<std::setw(3)<< iter_Rf << "\n";
-//             cusp::print(Rf);
+             //Tridiagonalize A first to T with the stopping condition for the function exp(-max(d)*alpha A)
+            auto Tf = krylovfunceigen.tridiag(func, A,  b, w2d, eps, 1.,  "universal");
+//          cusp::print(Tf);
+            iter = krylovfunceigen.get_iter();
+            std::cout << "    universal-iter: "<<std::setw(3)<< iter << "\n";
             
-            //make eigendecomposition of Rf = E_Rf  eval_Rf E_Rf^T 
-            cusp::array2d< double, cusp::host_memory> evecs_Rf(iter_Rf,iter_Rf);
-            cusp::array1d< double, cusp::host_memory> evals_Rf(iter_Rf);
-            cusp::lapack::stev(Rf.values.column(1), Rf.values.column(2), evals_Rf, evecs_Rf, 'V');
-            cusp::coo_matrix<int, double, cusp::host_memory> E_Rf, E_Rf_t;
-            cusp::convert(evecs_Rf, E_Rf);
-            cusp::transpose(E_Rf, E_Rf_t);
+            //make eigendecomposition of f(d T) e_1 = E_T f(d eval_T) E_T^T e_1
+            cusp::array2d< double, cusp::host_memory> evecs(iter,iter);
+            cusp::array1d< double, cusp::host_memory> evals(iter);
+            cusp::lapack::stev(Tf.values.column(1), Tf.values.column(2), evals, evecs, 'V');
+//             cusp::print(evecs);
             
+            //Compute c[l], v[l] and utlize them for x
+            std::vector<Container> v{iter,d}, c{iter,d}, fd{iter,d};
+            dg::HVec e_k(iter,0.); //unit vector e_k
+//             Container fd(d); // helper variable
+            dg::blas1::scal(x, 0.0);
             
-            //Compute h_k
-            dg::HVec e_1(iter_Rf,0.), e_k(e_1), y(e_1); //unit vector e_1
-            std::vector<dg::HVec> h{iter_Rf, e_1};
-            std::vector<Container> v{iter_Rf, d}; 
-            e_1[0] = 1.;
-            
-            dg::blas2::symv(E_Rf_t, e_1, y); //y = E_Rf^T e_1
-            dg::blas1::scal(x,0.0);
-            for( unsigned k=0; k<iter_Rf; k++)
+            //precompute v_l
+            for( unsigned k=0; k<iter; k++)
             {
+                dg::blas1::copy( 0, v[k]); // init sum
+                dg::blas1::copy( 0, c[k]); // init sum
+                //e_l
                 e_k[k] = 1.;
                 if (k>0) e_k[k-1]=0.;
-                
-                dg::blas1::pointwiseDot(e_k, y, y); //y = e_k * (E_Rf^T e_1)
-                dg::blas2::symv(E_Rf, y, h[k]);
-                
-                krylovfunceigend.normMbVy(d, Rf, h[k], v[k], b, krylovfunceigend.get_bnorm()); //v[k]=  ||b|| V_Rf h[k]
-                
-                //Solve 
-                iter= krylovfunceigen.solve(x_h, func, A, v[k], w2d, eps, 1., "universal"); // x_h = ||v_k|| V_Tf f(Tf lambda_Rf,k) v[k]
-                dg::blas1::axpby(1.0, x_h, 1.0, x);
-                std::cout << "    universal-iter-Tf: "<<std::setw(3)<< krylovfunceigen.get_iter() << "\n";
-
+                //compute v[l]
+                krylovfunceigen.normMbVy(A, Tf, e_k, v[k], b, 1.0); //v_k=  V e_k
             }
-            
-            //Tridiagonalize A first to T with the stopping condition for the function exp(-max(d)*alpha A)
+            //Compute v[k]
+            for( unsigned l=0; l<iter; l++)
+            {
+                for( unsigned i=0; i<iter; i++)
+                {                    
+                    dg::blas1::axpby( evals[i], d, 0., fd[i]); //fd_i = lambda_i d
+                    dg::blas1::transform(fd[i], fd[i], dg::mat::GyrolagK<double>(0.,-alpha)); //fd_i =  f(lambda_i d)
+                    for( unsigned k=0; k<iter; k++)
+                    {                 
+                        dg::blas1::pointwiseDot(evecs(i,l)*evecs(k,i), fd[i], v[k], 1.0, c[l]); //c_l += eps_{i,l} eps{k,i} f(lambda_i d) * v_k
+                    }
+                }
+                dg::blas1::axpby( dg::blas2::dot(c[l], b), v[l], 1., x); //x = (c_l.b) v_l //weights needed here?
+            }
             t.toc();
             time = t.diff();
             
+            //test function
+            x_h = dg::evaluate(lhs, g);
+            
+            //exact analytical solution not derivable for adjoint, using instead the exact solution of the non-adjoint operator
             x_exac = dg::evaluate(lhs, g);
             dg::blas1::scal(x_exac, funcs[u](ell_fac));
+            
+            
+            //Compute relative error in adjointness
+            double erel_adj = dg::blas2::dot( x_h, w2d, x_exac); //<f,exp(g,-A) f>
+            erel_adj= (erel_adj-dg::blas2::dot( x, w2d, x_h))/erel_adj; //(<f,exp(g,-A) f> -<exp(-A,g)f, f>)/<f,exp(g,-A) f>
+            std::cout << "    universal-error-adjointness: "<<erel_adj  << "\n";
 
+            
+            //Compute exact error for product exponential
+//             dg::blas1::axpby(ell_fac, d, 0.0, fd);
+//             dg::blas1::transform(fd, fd, dg::mat::GyrolagK<double>(0.,-alpha));
+//             dg::blas1::pointwiseDot(fd, x_exac, x_exac); //f(-alpha*(m^2+n^2) d) sin(m x) cos(n y)
         }
+//         if (u==2)
+//         {
+//             t.tic();
+//             //Tridiagonalize diagonal matrix D
+//             //test for b=1
+// //             Container b = dg::evaluate(dg::ONE(), g);
+// 
+//             auto Rf = krylovfunceigend.tridiag(func, d,  b, w2d, eps, 1.,  "universal");
+//             unsigned iter_Rf = krylovfunceigend.get_iter();
+//             std::cout << "    universal-iter-Rf: "<<std::setw(3)<< iter_Rf << "\n";
+// //             cusp::print(Rf);
+//             
+//             //make eigendecomposition of Rf = E_Rf  eval_Rf E_Rf^T 
+//             cusp::array2d< double, cusp::host_memory> evecs_Rf(iter_Rf,iter_Rf);
+//             cusp::array1d< double, cusp::host_memory> evals_Rf(iter_Rf);
+//             cusp::lapack::stev(Rf.values.column(1), Rf.values.column(2), evals_Rf, evecs_Rf, 'V');
+//             cusp::coo_matrix<int, double, cusp::host_memory> E_Rf, E_Rf_t;
+//             cusp::convert(evecs_Rf, E_Rf);
+//             cusp::transpose(E_Rf, E_Rf_t);
+//             
+//             
+//             //Compute h_k
+//             dg::HVec e_1(iter_Rf,0.), e_k(e_1), y(e_1); //unit vector e_1
+//             std::vector<dg::HVec> h{iter_Rf, e_1};
+//             std::vector<Container> v{iter_Rf, d}; 
+//             e_1[0] = 1.;
+//             
+//             dg::blas2::symv(E_Rf_t, e_1, y); //y = E_Rf^T e_1
+//             dg::blas1::scal(x,0.0);
+//             for( unsigned k=0; k<iter_Rf; k++)
+//             {
+//                 e_k[k] = 1.;
+//                 if (k>0) e_k[k-1]=0.;
+//                 
+//                 dg::blas1::pointwiseDot(e_k, y, y); //y = e_k * (E_Rf^T e_1)
+//                 dg::blas2::symv(E_Rf, y, h[k]);
+//                 
+//                 krylovfunceigend.normMbVy(d, Rf, h[k], v[k], b, krylovfunceigend.get_bnorm()); //v[k]=  ||b|| V_Rf h[k]
+//                 
+//                 //Solve 
+//                 iter= krylovfunceigen.solve(x_h, func, A, v[k], w2d, eps, 1., "universal"); // x_h = ||v_k|| V_Tf f(Tf lambda_Rf,k) v[k]
+//                 dg::blas1::axpby(1.0, x_h, 1.0, x);
+//                 std::cout << "    universal-iter-Tf: "<<std::setw(3)<< krylovfunceigen.get_iter() << "\n";
+// 
+//             }
+//             
+//             //Tridiagonalize A first to T with the stopping condition for the function exp(-max(d)*alpha A)
+//             t.toc();
+//             time = t.diff();
+//             
+//             //test function
+//             x_h = dg::evaluate(lhs, g);
+//             
+//             //exact analytical solution not derivable for adjoint, using instead the exact solution of the non-adjoint operator
+//             x_exac = dg::evaluate(lhs, g);
+//             dg::blas1::scal(x_exac, funcs[u](ell_fac));
+//             
+//             
+//             //Compute relative error in adjointness
+//             double erel_adj = dg::blas2::dot( x_h, w2d, x_exac); //<f,exp(g,-A) f>
+//             erel_adj= (erel_adj-dg::blas2::dot( x, w2d, x_h))/erel_adj; //(<f,exp(g,-A) f> -<exp(-A,g)f, f>)/<f,exp(g,-A) f>
+//             std::cout << "    universal-error-adjointness: "<<erel_adj  << "\n";
+// 
+//             
+//             //Compute exact error for product exponential
+// //             dg::blas1::axpby(ell_fac, d, 0.0, fd);
+// //             dg::blas1::transform(fd, fd, dg::mat::GyrolagK<double>(0.,-alpha));
+// //             dg::blas1::pointwiseDot(fd, x_exac, x_exac); //f(-alpha*(m^2+n^2) d) sin(m x) cos(n y)
+//         }
 //         if (u==3)
 //         {
 //             t.tic();
 //             for( unsigned k=0; k<x.size(); k++)
-//             {
+//             {x_exac
 // //                 A.set_chi(lambda_d[k]);
 //                 iter= krylovfunceigen.solve(x_h, func, A, b, w2d, eps, 1., "universal");
 // //                 std::cout << "    universal-iter[k]: "<<std::setw(3)<<iter <<"[" << k <<"]"<< "\n";
